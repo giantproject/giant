7411bc5f7e24740e6ba96b27180268adca8b26d1
zcatbear <jamieford34@gmail.com>
2017-02-03 10:49:59 -0600

    Name Change

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='giant_repl',
+setup(name='giant_one',
       version='0.1',
-      py_modules=['repl'],
+      py_modules=['giant_one'],
       install_requires=['Click', 'requests', 'python-whois'],
       entry_points='''
       [console_scripts]
-      repl=repl:cli'''
+      giant_one=giant_one:cli'''
       )
