17d6a8c226a987ddf35e21c61a5bbbe152f0e3ea
zcatbear <jamieford34@gmail.com>
2017-04-03 22:41:03 -0500

    Added database searching logic for ipinfo

--- containers/ipinfo/ipinfo.py
+++ containers/ipinfo/ipinfo.py
@@ -3,6 +3,8 @@ from flask import Flask, redirect, url_for, render_template, request
 import json
 import requests
 from pymongo import MongoClient
+import datetime
+
 app = Flask(__name__)
 
 client = MongoClient(os.environ['DB_1_PORT_27017_TCP_ADDR'], 27017)
@@ -15,9 +17,15 @@ def help():
 @app.route('/ipinfo')
 @app.route('/ipinfo/<ip>')
 def ipinfo(ip=''):
+    findResult = findRecord(ip)
+    id = findResult.get('_id')
+    if (findResult is not None):
+        return {"status":"Found", "id" = str(id), "result": findResult}
     lookup = "http://ipinfo.io/" + ip
     result = requests.get(lookup)
-    insertionResult = insertRecord(json.loads(result.text))
+    result = json.loads(result.text)
+    result['time'] = datetime.now()
+    insertionResult = insertRecord(result)
     if (insertionResult['status'] != "Success"):
         return insertionResult
     insertionResult['result'] = json.loads(result.text)
@@ -29,6 +37,17 @@ def insertRecord(record):
         return {"status":"Success", "id":str(id)}
     except Exception as e:
         return {"status":"Failure", "error": str(e)}
+
+def findRecord(ip):
+    try:
+        record = db.ipinfo.find_one({'ip':ip})
+        if (record is not None):
+            return record
+        else:
+            return None
+    except:
+        return None
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5002, debug=True)
 
