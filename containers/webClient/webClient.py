2a85db913a5d7165651b83e8fe3979b13a88e015
Hunter "The Boss" Hawthorne <hunter.hawthorne1@gmail.com>
2017-04-06 18:53:29 -0500

    webclient is connecting

--- containers/webClient/webClient.py
+++ containers/webClient/webClient.py
@@ -5,26 +5,13 @@ import requests
 
 app = Flask(__name__)
 
-'''client = MongoClient(os.environ['DB_1_PORT_27017_TCP_ADDR'], 27017)'''
-'''db= client.db'''
-
-ipInfoURL = os.environ['IPINFO_1_PORT_5002_TCP_ADDR']
-
 
 @app.route('/')
 def home():
     return "hello"
 
 
-# ipinfoURL = os.environ['IPINFO_1_PORT_5002_TCP_ADDR']
-# render_template('home.html')
-
-@app.route('/ipinfo')
-def ipinfo():
-    ip = request.args.get('ip')
-    r = requests.get(ipInfoURL + '/ipinfo/' + ip)
-    return r.json()
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5002, debug=True)
+    app.run(host='0.0.0.0', port=5003, debug=True)
