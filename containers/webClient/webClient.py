0457d2cddec86c550c03bcc6201c0d0662575e40
Hunter "The Boss" Hawthorne <hunter.hawthorne1@gmail.com>
2017-04-06 19:33:44 -0500

    webclient updated

--- containers/webClient/webClient.py
+++ containers/webClient/webClient.py
@@ -4,13 +4,17 @@ import json
 import requests
 
 app = Flask(__name__)
-
+ipInfoIp = os.environ['TODO_CONTAINERS_IPINFO_1_PORT_5002_TCP_ADDR']
 
 @app.route('/')
 def home():
-    return "hello"
+    return render_template("home.html")
 
-
+@app.route('/ipinfo', methods=['POST'])
+def ipInfo():
+    ip = request.form['ip']
+    return request.get(ipInfoIp+':5002/ipinfo/'+ip)
+	
 
 
 if __name__ == "__main__":
