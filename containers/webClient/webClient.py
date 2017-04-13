450b61d2b4c97b0ee32e061b324c5f6284733fa2
Hunter "The Boss" Hawthorne <hunter.hawthorne1@gmail.com>
2017-04-13 14:23:33 -0500

    web client works with pywhois and ipinfo

--- containers/webClient/webClient.py
+++ containers/webClient/webClient.py
@@ -2,20 +2,28 @@ import os
 from flask import Flask, redirect, url_for, render_template, request
 import json
 import requests
+import json
 
 app = Flask(__name__)
 ipInfoIp = os.environ['IPINFO_1_PORT_5002_TCP_ADDR']
+pywhoisIp = os.environ['PYWHOIS_1_PORT_5001_TCP_ADDR']
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    
+    return render_template("home.html", jsonIp="")
 
 @app.route('/ipinfo', methods=['POST'])
 def ipInfo():
     ip = request.form['ip']
-    return requests.get('http://'+ipInfoIp+':5002/ipinfo/'+ip).text
+    unparsed_json = requests.get('http://'+ipInfoIp+':5002/ipinfo/'+ip).text
+    return render_template('displayip.html', parsed_json = unparsed_json)
 	
-
+@app.route('/pywhois', methods=['POST'])
+def pywhois():
+    domain = request.form['domain']
+    unparsed_json = requests.get('http://'+pywhoisIp+':5001/pywhois/'+domain).text
+    return render_template('displaydomain.html', parsed_json = unparsed_json)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5003, debug=True)
